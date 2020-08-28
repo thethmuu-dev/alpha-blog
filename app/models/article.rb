a794909172c1b35ecb7bb09bceeff4a923c11d95
Thet Hmuu Eain Soe <richardfromsv@gmail.com>
2020-08-28 11:56:21 +0630

    Add Secure Password

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    belongs_to :user
+
     validates :title, presence: true, length: {minimum: 6}
     validates :description, presence: true, length: {maximum: 60000}
 end
